846ff1836502ff0f7304eea415fbb67c32b80979
markzzw <453646474@qq.com>
2019-07-10 16:03:22 +0800

    feat: add min & max limitation

--- jqueryComponent/animated-process/process.js
+++ jqueryComponent/animated-process/process.js
@@ -1,13 +1,15 @@
-var processBar = function (barSelector) {
+var processBar = function (barSelector, minPercentNumber) {
+  var minPer = minPercentNumber || 0;
+  var maxPer = 100 - minPer;
   $(barSelector).find('.process-bar').each(function (_, item) {
     var $item = $(item);
     var backgroundColor = $item.attr('data-bg') || 'none';
-    var width = $item.attr('data-percent');
+    var width = getWidth($item.attr('data-percent') * 1, maxPer, minPer);
     var speed = $item.attr('data-speed') * 1;
     $item.css({ 'background': backgroundColor })
       .animate(
         {
-          width: width,
+          width: width + '%',
           opacity: 1
         }
         , speed || 1000
@@ -15,14 +17,23 @@ var processBar = function (barSelector) {
   })
 }
 
-var processNumber = function (numberSelectors) {
+function getWidth(width, max, min) {
+  if (width < min) return min;
+  if (width > max) return max;
+  return width;
+}
+
+var processNumber = function (numberSelectors, minPercentNumber) {
+  var minPer = minPercentNumber || 0;
+  var maxPer = 100 - minPer;
   $(numberSelectors).each(function (_, item) {
     var $item = $(item);
     var suffix = $item.attr('data-suffix');
     var during = $item.attr('data-during') * 1;
     var number = $item.attr('data-num') * 1;
     var speed = during / number;
-    $item.css({ width: number - 5 + '%' });
+    var width = getWidth(number, maxPer, minPer);
+    $item.css({ width: width - 5 + '%' });
     $item.attr('data-curNumber', 0);
     var timer = setInterval(function () {
       var curNumber = $item.attr('data-curNumber') * 1;
